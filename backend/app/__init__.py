0ca8ff354c911ae8d844708b705acc7fd7a16b0f
kazuma <kazumamiyoshi0311@gmail.com>
2024-09-16 15:06:25 +0900

    backend commit

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -16,7 +16,7 @@ csrf = CSRFProtect()
 
 def create_app():
   app = Flask(__name__)
-  CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
+  CORS(app, supports_credentials=True, origins=["http://localhost:3000","https://community-app-lmxz.vercel.app"])
   app.config.from_object(Config)
   csrf.init_app(app)
   
